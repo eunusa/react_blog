25d4513eeb1966650e71e7073e83360bc241abd4
saeunwoo <66991444+eunusa@users.noreply.github.com>
2022-03-26 21:33:34 +0900

    Add files via upload
    useEffect 훅 컴포넌트가 실행될때 조건을 실행할 수 있다.
    
    return 값을 주면 컴포넌트가 종료될때 조건을 실행 할 수 있다.

--- react_shop/Detail.js
+++ react_shop/Detail.js
@@ -1,4 +1,4 @@
-import react, { useState } from 'react';
+import react, { useEffect, useState } from 'react';
 import { useHistory, useParams } from 'react-router-dom';
 import data from './data.js';
 import styled from 'styled-components';
@@ -19,20 +19,35 @@ let Titlelist = styled.h4`
 
 
 
+
+
+
 function Detail(props){
+
+    useEffect(()=>{
+        // let setitemea = setTimeout(()=>{
+        // itemeaC(true);
+        // },2000)
+
+        return (()=>{})/* 컴포넌트가 사라질때 실행된다. */
+        /* useEffect는 순서대로 실행이 된다. */
+    });
+
 
     let { id } = useParams();
     let history = useHistory(); /* 방문기록이 남아있는 히스토리 useHistory 훅 history.push('/') 여길로 이동할수 있다. */
     let idList = data;
+    let [itemea,itemeaC] = useState(false)
 
     return(
     <div className="container">
         <Boxlist>
             <Titlelist className='red' >테스트</Titlelist> {/* props문법 보낼이름={변수명} 보낼이름="일반문자" */}
         </Boxlist>
-        <div className='my-alert2'>
-            <p>재고가 얼마 남지 않았습니다.</p>
-        </div>
+        { itemea === true
+          ?<Itemeaa />
+          : null
+        }
         <div className="row">
         <div className="col-md-6">
             <img src="https://codingapple1.github.io/shop/shoes1.jpg" width="100%" />
@@ -51,4 +66,9 @@ function Detail(props){
 )
 }
 
+function Itemeaa(){
+return(<div className='my-alert2'>
+            <p>재고가 얼마 남지 않았습니다.</p>
+        </div>)
+}
 export default Detail;
