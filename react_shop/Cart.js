be5516144a8fb7c24d08a3c36a4a6c9ff0dbce5c
saeunwoo <66991444+eunusa@users.noreply.github.com>
2022-04-05 22:03:21 +0900

    Add files via upload
    reducer 2개 이상이 필요하다면 combineReducers() 함수안에 reducer를 여러게 넣고 사용한다.
    reducer를 props로 만들때 오브젝트안에 넣으면 된다.

--- react_shop/Cart.js
+++ react_shop/Cart.js
@@ -3,12 +3,17 @@ import {Table} from "react-bootstrap"
 import {connect} from "react-redux"
 
 
-
+function itemCart(state){
+  return{
+    cartName : state.reducer,
+    popup : state.reducer2 //state를 cartName으로 쓰겠습니다.
+  }
+} //redux store 데이터를 가져와서 props로 변환해주는 함수
 
 function Cart(props){
     return(
         <div>
-            <Table striped bordered hover>
+            <Table  className='mt-5' striped bordered hover>
   <thead>
     <tr>
       <th>#</th>
@@ -31,13 +36,17 @@ function Cart(props){
       }
   </tbody>
 </Table>
+
+      {props.popup === true
+        ?(<div className="my-alert2">
+          <p>지금 구매하시면 신규할인 20%</p>
+          <button onClick={()=>{props.dispatch({type : 'popUpclose'})}}>닫기</button>
+          </div>)
+        :null
+      } {/* 연습으로 만든거지 useState를 사용해서 하면 짱편한다. */}
 </div>
     )
 }
 
-function itemCart(state){
-  return{
-    cartName : state
-  }
-}
-export default connect(itemCart)(Cart);
+
+export default connect(itemCart)(Cart); // itemCart, Cart에 redux 연결
--- react_shop/index.js
+++ react_shop/index.js
@@ -8,9 +8,21 @@ import { BrowserRouter } from 'react-router-dom'; // 라이브러리 이름
 //HashRouter 사용 가능
 
 import {Provider} from 'react-redux';
-import { createStore } from 'redux';
+import { combineReducers, createStore } from 'redux';
 
 let 초기값 = [{id :0, name :'멋진신발', quan :2},{id :1, name :'아디다스', quan :4}];
+
+let popup = true;
+
+function reducer2(state = popup, 액션){
+  if(액션.type ==='popUpclose'){
+    let popup = state;
+    popup = false;
+    return popup
+  } else {return state}
+} //연습으로 만든거지 useState를 사용해서 하면 짱편한다.
+
+
 
 function reducer(state = 초기값, 액션){
   if (액션.type === '수량증가'){
@@ -28,7 +40,7 @@ function reducer(state = 초기값, 액션){
 }
 
 
-let store = createStore(reducer);
+let store = createStore(combineReducers({reducer,reducer2}));
 
 ReactDOM.render(
   <React.StrictMode>
